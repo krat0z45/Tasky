223ca50296ae16f59e977844258de21ceaa8d07d
Diego Esparza <motokrat0z@gmail.com>
2026-03-25 20:09:44 -0600

    Fix Prisma client generation for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // !! ADVERTENCIA !!
-    // Permite que la producción se compile correctamente incluso si hay errores de TypeScript.
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Permite que la producción se compile incluso si hay advertencias de ESLint.
-    ignoreDuringBuilds: true,
   },
 };
 
